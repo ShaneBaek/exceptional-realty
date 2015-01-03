8cbe9e6b27b13e50683b3ddb08b7415f1d7821ca
Shane Baek <hobas9626@gmail.com>
2015-01-03 12:03:55 -0500

    added media query js for mobile nav menu

---
+++ js/main.js
@@ -0,0 +1,15 @@
+$(function() {
+
+ //////Mobile Menu/////
+ $('a.menu-icon').click(function() {
+ 	$('#menu-links').slideToggle();
+ });
+
+ /////Fix Hidden Links on window resize////
+ $(window).resize(function() {
+ 	if ($(window).width() > 700) {
+ 		$('#menu-links').removeAttr('style');
+ 	}
+ });
+
+});
